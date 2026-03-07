549afd36ffd535cfd6c6a8781f94fd3affe37358
academialibredegeomatica <pueblosvivos1@gmail.com>
2026-03-07 13:22:41 +0000

    rter

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,22 +2,22 @@ import { defineCollection, z } from 'astro:content';
 
 const docs = defineCollection({
   schema: z.object({
-    title: z.string(),
-    empresa: z.string().optional(),
-    tipo: z.string().optional(),
-    botonTexto: z.string().optional(),
-    image: z.string().optional(), // <--- Cambiado de 'image()' a 'z.string()'
-    weight: z.number().optional(),
-  }),
-});
+      title: z.string(),
+          empresa: z.string().optional(),
+              tipo: z.string().optional(),
+                  botonTexto: z.string().optional(),
+                      image: z.string().optional(), // <--- Cambiado de 'image()' a 'z.string()'
+                          weight: z.number().optional(),
+                            }),
+                            });
 
-const indexCards = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    description: z.string().optional(),
-    image: z.string().optional(), // <--- También aquí como string
-    link: z.string().optional(),
-  }),
-});
+                            const indexCards = defineCollection({
+                              schema: z.object({
+                                  title: z.string(),
+                                      description: z.string().optional(),
+                                          image: z.string().optional(), // <--- También aquí como string
+                                              link: z.string().optional(),
+                                                }),
+                                                });
 
-export const collections = { 'docs': docs, 'indexCards': indexCards };
+                                                export const collections = { 'docs': docs, 'indexCards': indexCards };
--- src/content/home/-index.md
+++ src/content/home/-index.md
@@ -1,5 +1,5 @@
 ---
-image: "@assets/astrogon-star.svg"
+image: ""
 imageAlt: Astrogon logo
 title: Academia Libre de Geomática
 content: "**Plataforma de intercambio de cursos en Geomática y más...**"
